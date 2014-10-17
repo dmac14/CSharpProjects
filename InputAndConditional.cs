faa462df27c55e8b1ed53a6f3705f47aa9f7d4fb
Daniel McDonald <choosemcd14@gmail.com>
2014-10-17 12:14:40 -0700

    (1) Input and Condition
    A program which receives a user's input, checks to see if it matches a
    string of numbers, and then outputs a message accordingly

---
+++ InputAndConditional.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProjects
+{
+    public class Program
+    {
+        //Program to read input from readers and makes use of conditional if statement.
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor=ConsoleColor.Red;
+            Console.WriteLine("Type in the numbers and press Return: ");
+            string numbers = Console.ReadLine();
+
+            if (numbers=="4 8 15 16 23 42")
+            {
+                Console.WriteLine("Recalibrating energy core ... Complete.");
+            }
+            else
+            {
+                Console.WriteLine("FAILURE!");
+                Console.Beep();
+            }
+            
+            //Wait for user to acknowledge the results.
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+}
+
+
