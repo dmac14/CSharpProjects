82b9a0665c9f3289228b6e58865a153e5a8ed21c
Daniel McDonald <choosemcd14@gmail.com>
2014-10-20 11:50:19 -0700

    (8) LINQ with a DB
    A program which takes in user inputs, validates them, and then stores
    them in a corresponding database. Then checks to confirm the data was
    committed.
    
    The database (myDB) has one table called Sales with four columns:
    1. SalesID (the identity/primary key of the table, int)
    2. SalesAmount (smallmoney)
    3. SalesDate (smalldatetime)
    4. TaxAmount (smallmoney)

---
+++ LINQ_DB.cs
@@ -0,0 +1,70 @@
+﻿using TaxManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProjects
+{
+    class LINQwithDB
+    {
+        static void Main(string[] args)
+        {
+            myDBEntities db = new myDBEntities();
+
+            Sale newSale = new Sale();
+            Console.WriteLine("Record a sale...");
+
+
+            Console.WriteLine("Enter Date: ");
+            string inputDate = (Console.ReadLine());
+            inputDate = inputDate.Trim();
+            DateTime salesDate;
+            if (!DateTime.TryParse(inputDate,out salesDate))
+	        {
+                Console.WriteLine("Error, not a valid date! Press Enter to terminate...");
+                Console.Read();
+                return;
+	        }
+            newSale.SalesDate = DateTime.Parse(inputDate);
+
+
+            Console.WriteLine("Enter Amount: ");
+            string inputAmount = Console.ReadLine();
+            inputAmount = inputAmount.Trim();
+            decimal salesAmount;
+            if(!decimal.TryParse(inputAmount,out salesAmount))
+            {
+                Console.WriteLine("Error, not a valid amount! Press Enter to terminate...");
+                Console.Read();
+                return;
+            }
+            newSale.SalesAmount = decimal.Parse(inputAmount);
+
+            newSale.TaxAmount = Tax.CalculateTax(newSale.SalesAmount);
+
+            db.Sales.Add(newSale);
+            db.SaveChanges();
+
+            var sales = db.Sales.Select(s => s);
+            foreach (var item in sales)
+            {
+                Console.WriteLine("{0} -- SaleAmount: {1:C} Tax: {2:C}",item.SalesDate.ToShortDateString(),item.SalesAmount,item.TaxAmount);
+            }
+            
+            //Wait for user to acknowledge the results.
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+
+    public class Tax
+    {
+        public static decimal CalculateTax(decimal amount)
+        {
+            decimal taxAmount = amount * .0825M;
+            return taxAmount;
+        }
+    }
+}
